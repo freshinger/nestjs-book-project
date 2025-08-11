f8ba12479dd94b91739f044d49ccf181df107bec
Daniel <daniel_ciecior@yahoo.de>
2025-08-11 11:10:44 +0200

    switched from sqlite to postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from 'books/entities/book.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite', // <--- Specify your database type here
-      database: 'database.sqlite', // <--- Path to your DB file (for SQLite) or database name
-
+      type: 'postgres', // <--- Specify your database type here
+      url: process.env.DATABASE_URL, // <--- Path to your DB file (for SQLite) or database name
+      ssl: {
+        rejectUnauthorized: false,
+      },
       entities: [Book], // <--- **IMPORTANT:** This array will hold your Entity classes (e.g., [User, Product])
       //     You can also use a glob pattern like ['dist/**/*.entity{.ts,.js}']
       //     but explicitly listing them is often clearer for smaller projects.
